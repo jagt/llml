eda6e7c3c79e0081766079c56d03239313b13490
jagt <jagt@live.com>
2016-09-21 02:04:49 +0800

    more scaffolding

---
+++ llml/llMiscs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// Misc things
+
+namespace llml
+{
+	/// base exception class for all llml throw
+	public class llException : Exception
+	{
+		public llException(string msg) : base(msg) { }
+	}
+}
