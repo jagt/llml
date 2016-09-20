af38692a094b57ca71010630aea0c0a3b76c6f1c
jagt <jagt@live.com>
2016-09-21 00:29:29 +0800

    protobuild scaffolding

---
+++ llml/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace llml
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello llml!");
+        }
+    }
+}
+
